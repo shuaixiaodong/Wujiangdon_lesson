a0cc439bd59c34a96b381f9c9012e962c333067b
shuaixiaodong <1550544548@qq.com>
2019-05-30 11:22:21 +0800

    vue

---
+++ vue-learn/vue-demo/element/src/components/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <Header></Header>
+    <Slide></Slide>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Header from '@/components/common/Header'
+import Slide from '@/components/common/Slide'
+export default {
+  name: 'mainContent',
+  data () {
+    return {
+      msg: 'hello world'
+    }
+  },
+  components: {
+    Header,
+    Slide
+  }
+}
+</script>
