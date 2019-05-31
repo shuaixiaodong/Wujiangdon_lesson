35e198f6182fc1bdb59437a3195bcf0084cf063e
shuaixiaodong <1550544548@qq.com>
2019-05-31 23:24:24 +0800

    vue路由

---
+++ vue-learn/vue-router/routerLearn/src/components/LookOne.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="look-one">{{msg}}
+    <div class="go-to" @click='goTwo'>去look2</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LookOne',
+  data () {
+    return {
+      msg: '你瞅啥？'
+    }
+  },
+  methods: {
+    goTwo: function () {
+      this.$router.push({ name: 'LookTwo', param: {id: 123} })
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    console.log(from, to)
+    next()
+  },
+  beforeRouteLeave (to, from, next) {
+    console.log(from, to)
+    next()
+  }
+}
+</script>
